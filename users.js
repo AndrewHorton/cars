4cfa834f8955ed9353ee4ed29f969e47d14f69c8
AndrewHorton <67080061+AndrewHorton@users.noreply.github.com>
2020-07-01 13:24:24 -0700

    Add files via upload

---
+++ users.js
@@ -0,0 +1,16 @@
+//Users
+user = {
+  guest:['guestreview'],
+  registered:['userID', 'permissions', 'details']
+
+}
+
+
+permissions =['registeredreview', 'buy', 'List']
+guestreview =['description', 'details', 'photos']
+registeredreview =['description','details','photos', 'report', 'testdrive']
+buy =['loan', 'offer', 'creditCard', 'crypto', 'date']
+list =['description', 'details', 'photos', 'price', 'date', 'remove', 'update']
+details = ['profile', 'firstName', 'middleInitial', 'lastName', 'contactDetails']
+profile =['avatar']
+contactDetails=['phone', 'email', 'address']
